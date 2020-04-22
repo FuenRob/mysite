808af27478b4da429e052d17c7368769c8daeac4
FuenRob <fuenrob@gmail.com>
2020-04-22 19:18:30 +0200

    Add banner cookie policies

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import CookieConsent from "react-cookie-consent";
 import Header from "./header"
 import Footer from "./footer"
 
@@ -26,6 +27,9 @@ export default ({ children }) => {
             <Header title={data.site.siteMetadata.title} menuLinks={data.site.siteMetadata.menuLinks}></Header>
             <div className="container">{children}</div>
             <Footer />
+            <CookieConsent buttonText="Lo entiendo" buttonStyle={{ background: "rebeccapurple", color: "white", fontSize: "13px" }}>
+                Este sitio web utiliza cookies para mejorar la experiencia del usuario.
+            </CookieConsent>
         </div>
     )
 }
