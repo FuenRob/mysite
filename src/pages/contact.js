2fd06e701e14eabf289b487aebfa12f7c1bf8730
FuenRob <fuenrob@gmail.com>
2020-05-19 20:34:07 +0200

    Change the form provider

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,10 @@
 import React from "react"
-import { graphql } from "gatsby"
-import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
 import { Link } from "gatsby"
 
-export default ({data}) => (
+export default () => (
     <Layout>
         <div>
-          <Helmet>
-            <script src="https://www.google.com/recaptcha/api.js"></script>
-          </Helmet>
             <h1 className="title">
               Contacto
             </h1>
@@ -17,7 +12,7 @@ export default ({data}) => (
                 Si tienes cualquier consulta o problema puedes escribirme en cualquiera de las redes sociales que están 
                 en el footer o en este formulario de contacto. Te respondere lo más rápido posible.
             </p>
-            <form className="flex" action="https://getform.io/f/0cbdffcd-da6a-40ae-873c-1843ad03f253" method="POST">
+            <form className="flex" action="https://formspree.io/xgenolbq" method="POST">
               <div className="col-medium">
                 <div className="formgroup">
                   <label htmlFor="name">
@@ -28,7 +23,7 @@ export default ({data}) => (
                 <div className="formgroup">
                   <label htmlFor="email">
                     Correo electrónico*
-                    <input className="formcontrol" type="email" name="email" id="email" required />
+                    <input className="formcontrol" type="email" name="_replyto" id="email" required />
                   </label>
                 </div>
                 <div className="formgroup">
@@ -49,9 +44,6 @@ export default ({data}) => (
               <div className="col-fullwidth text-center">
                <p className="small-text">El * indica que el campo es requerido.</p>
                 <div className="formgroup">
-                  <div className="g-recaptcha text-center" data-sitekey={data.site.siteMetadata.contact.reCAPTCHA_key}></div>
-                </div>
-                <div className="formgroup">
                   <input type="checkbox" name="acceptConditions" required /> Acepto la <Link to="/politica-de-privacidad">política de privacidad</Link>.
                 </div>
                 <button type="submit" className="btn btn-primary">Enviar</button>
@@ -60,15 +52,3 @@ export default ({data}) => (
         </div>
     </Layout>
 )
-
-export const query = graphql`
-  query {
-    site {
-      siteMetadata{
-        contact {
-          reCAPTCHA_key
-        }
-      }
-    }
-  }
-`
